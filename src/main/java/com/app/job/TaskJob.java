c82a99c508a0005a76a0d04ddb998993bf365940
zengxiangyi <zengxiangyi@126.com>
2020-09-06 12:11:07 +0800

    -- 保存代码

--- src/main/java/com/app/job/TaskJob.java
+++ src/main/java/com/app/job/TaskJob.java
@@ -1,13 +1,20 @@
 package com.app.job;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.app.util.JsonUtil;
+
 @Component
 public class TaskJob {
+	private static Logger log = LoggerFactory.getLogger(TaskJob.class);
 	
 	@Scheduled(cron = "0/5 * * * * ?")
 	public void fixTimeExecution() {
-		
+		log.info(JsonUtil.toJsonStr(LocalDateTime.now()));
 	}
 }
--- src/main/java/com/app/web/SendData.java
+++ src/main/java/com/app/web/SendData.java
@@ -1,6 +1,5 @@
 package com.app.web;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
